5da7ca2fb378f85b7870ad0f25b039b8e2ee3fc4
游灵 <waylin89@126.com>
2026-09-11 01:03:22 +0800

    更新文件

---
+++ trunk/configs/boards/JSH-03/board.h
@@ -0,0 +1,30 @@
+/* JSH-03 */
+
+#define BOARD_PID		"JSH-03"
+#define BOARD_NAME		"JSH-03"
+#define BOARD_DESC		"JSH-03 Wireless Router"
+#define BOARD_VENDOR_NAME	"JSH-03"
+#define BOARD_VENDOR_URL	"http://www.techmes.cn/"
+#define BOARD_MODEL_URL		"http://www.techmes.cn/"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	38
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_SW2G
+#define BOARD_GPIO_LED_POWER	43
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_WAN
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
+#define BOARD_NUM_ETH_EPHY	3
